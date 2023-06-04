b6d2ed7b9a21c08d12b41aa435b1ffc7d87784da
Aldo Costa <aldo.costa@web.de>
2023-06-04 11:12:28 +0200

    - fixed header scaling

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -2,21 +2,27 @@ import Link from "next/link";
 
 export default function Header(){
     return (
-        <div className="flex w-full justify-between m-0">
+        <div className="max-w-screen-md max-h-screen-md overflow-hidden flex justify-between m-0">
             <Link href="/" className="m-5">
-            <img src="/logo.png"
-                alt="Logo"
-                className= "dark:brightness-0 w-20 h-20 dark:invert transition glow duration-500 hover:drop-shadow-[0_7px_11px_rgba(100,100,100,1)]"
-            />
+                <img
+                    src="/logo.png"
+                    alt="Logo"
+                    className="dark:brightness-0 w-20 max-w-full max-h-full dark:invert transition glow duration-500 hover:drop-shadow-[0_7px_11px_rgba(100,100,100,1)]"
+                />
             </Link>
 
-            <h2 className="text-2xl font-bold mt-7  text-center w-44 h-10 rounded border-2 transition glow duration-500 hover:drop-shadow-[0_7px_11px_rgba(100,100,100,5)]">Store Scanner</h2>
+            <h2 className="flex-shrink-0 overflow-hidden text-overflow-ellipsis text-xl font-bold mt-7 text-center w-44 h-10 rounded border-2 transition glow duration-500 hover:drop-shadow-[0_7px_11px_rgba(100,100,100,5)]">
+                Store Scanner
+            </h2>
+
             <Link href="/account" className="m-5">
-            <img src="/ProfilePic.jpeg"
-                alt="Profile Picture"
-                className="w-20 h-20 rounded-full transition filter duration-500 hover:drop-shadow-[0_7px_11px_rgba(100,100,100,1)]"
-            />
+                <img
+                    src="/ProfilePic.jpeg"
+                    alt="Profile Picture"
+                    className="w-20 max-w-full max-h-full rounded-full transition filter duration-500 hover:drop-shadow-[0_7px_11px_rgba(100,100,100,1)]"
+                />
             </Link>
         </div>
+
     );
 }
