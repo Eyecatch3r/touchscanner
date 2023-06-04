719c758d6541eb7867c23081afd3c61ad287450e
Aldo Costa <aldo.costa@web.de>
2023-06-04 02:07:02 +0200

    - fixed custom css colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,14 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'darkCard': '#1d1d1d',
+      'darkTextSecondary': '#c5c5c5',
+      'whiteCard': '#EBEBEB',
+      'whiteTextSecondary': '#7a7a7a'
+    }
   },
   plugins: [],
 }
