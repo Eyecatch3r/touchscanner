2364a24fd10f39e96bfcab59a29321b99da10963
MCKerim <MCKerim@gmx.de>
2023-06-05 21:02:42 +0200

    Added space at bottom of page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -102,6 +102,9 @@ export default function Home() {
               <ProductCard key={index} {...product} />
           ))}
       </animated.div>
+      <div className="mb-10">
+
+      </div>
     </main>
-  )
+  );
 }
